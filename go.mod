8e27d8ffb46bfcc3cb0627433b8bb4767742e57a
Maxim Khludeev <m.a.khludeev@esoft.local>
2025-03-02 14:19:12 +0500

    feat: first step

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/Supkaa/supimg
 
 go 1.22.5
 
+require github.com/davidbyttow/govips/v2 v2.16.0
+
 require (
-	github.com/davidbyttow/govips/v2 v2.15.0 // indirect
-	golang.org/x/image v0.19.0 // indirect
-	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,132 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
 	"github.com/davidbyttow/govips/v2/vips"
-	"log"
 )
 
+// w - image output width
+// h - image output height
+// q - image output quality
+// ext - image output extension
+// wmt - watermark text
+// wmpos - watermark position
+// wmclr- watermark color
+// wmop - watermark opacity
 func main() {
-	img, err := vips.NewImageFromFile("")
-	if err != nil {
-		log.Fatal(err)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		widthQuery := query.Get("w")
+		heightQuery := query.Get("h")
+		qualityQuery := query.Get("q")
+		// extension := query.Get("ext")
+		quality := 100
+		if qualityQuery != "" {
+			q, err := strconv.Atoi(qualityQuery)
+			if err == nil {
+				quality = q
+			}
+		}
+
+		pathToStorage := strings.ReplaceAll(r.URL.Path, "content/cluster", "mnt/gluster")
+		img, err := vips.NewImageFromFile(fmt.Sprintf(".%s", pathToStorage))
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		width, height := getImgWidthAndHeightFromQuery(img.Width(), img.Height(), widthQuery, heightQuery)
+		// scaleFactor := math.Min(float64(width)/float64(img.Width()), float64(height)/float64(img.Height()))
+		// img.Resize(scaleFactor, vips.KernelAuto)
+		// img.ResizeWithVScale(float64(width)/float64(img.Width()), float64(height)/float64(img.Height()), vips.KernelAuto)
+		if img.Width() > img.Height() {
+			img.Thumbnail(width, height, vips.InterestingNone)
+		} else {
+			img.Thumbnail(height, width, vips.InterestingNone)
+		}
+		if img.Bands() < 3 {
+			_ = img.ToColorSpace(vips.InterpretationSRGB)
+		}
+		if img.Bands() == 4 {
+			img.ExtractBandToImage(3, 1)
+			_ = img.ExtractBand(0, 3)
+		}
+		// vips.LabelParams{}
+		img.Label(&vips.LabelParams{
+			Text: "etagi",
+			Font: "sans 12",
+			Width: vips.Scalar{
+				Value:    0.5,
+				Relative: true,
+			},
+			Height: vips.Scalar{
+				Value:    0.5,
+				Relative: true,
+			},
+			OffsetX: vips.Scalar{
+				Value:    0,
+				Relative: true,
+			},
+			OffsetY: vips.Scalar{
+				Value:    0,
+				Relative: true,
+			},
+			Color: vips.Color{
+				R: 0,
+				G: 0,
+				B: 0,
+			},
+			Alignment: vips.AlignLow,
+		})
+		b, _, _ := img.Export(&vips.ExportParams{
+			Format:  vips.ImageTypeJPEG,
+			Quality: quality,
+		})
+		w.WriteHeader(200)
+		w.Write(b)
+		// w.Write([]byte(fmt.Sprintf("path: %s, width: %s, height: %s, quality: %s, extension: %s", r.URL.Path, width, height, quality, extension)))
+	})
+
+	http.ListenAndServe(":8080", mux)
+
+	// img, err := vips.NewImageFromFile("")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// img.Label(&vips.LabelParams{
+	// 	Text:      "",
+	// 	Font:      "",
+	// 	Width:     vips.Scalar{},
+	// 	Height:    vips.Scalar{},
+	// 	OffsetX:   vips.Scalar{},
+	// 	OffsetY:   vips.Scalar{},
+	// 	Opacity:   0,
+	// 	Color:     vips.Color{},
+	// 	Alignment: 0,
+	// })
+}
+
+func getImgWidthAndHeightFromQuery(width, height int, widthQuery, heightQuery string) (int, int) {
+	if widthQuery != "" && heightQuery != "" {
+		w, err := strconv.Atoi(widthQuery)
+		if err != nil {
+			return width, height
+		}
+
+		h, err := strconv.Atoi(heightQuery)
+		if err != nil {
+			return width, height
+		}
+
+		return w, h
 	}
 
-	img.Label(&vips.LabelParams{
-		Text:      "",
-		Font:      "",
-		Width:     vips.Scalar{},
-		Height:    vips.Scalar{},
-		OffsetX:   vips.Scalar{},
-		OffsetY:   vips.Scalar{},
-		Opacity:   0,
-		Color:     vips.Color{},
-		Alignment: 0,
-	})
+	return width, height
 }
